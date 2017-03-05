d1e75e81d542bbda3d7d93dc6b8ea57fef13d3d0
Yoan Dawn <Yoan Dawn>
2017-03-05 21:17:02 +0200

    Possibly last commit

--- src/main/java/io/github/rsmake/EcoKill/Eco.java
+++ src/main/java/io/github/rsmake/EcoKill/Eco.java
@@ -1,8 +1,5 @@
 package io.github.rsmake.EcoKill;
 
-import net.milkbowl.vault.chat.Chat;
-import net.milkbowl.vault.economy.Economy;
-import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -19,11 +15,9 @@ import java.util.logging.Logger;
 public class Eco extends JavaPlugin implements Listener{
 
     public final static Logger logger = Logger.getLogger("minecraft");
-    public static Economy econ = null;
-    public static Permission perms = null;
-    public static Chat chat = null;
-    private double moneyOnKill = 5;
-    private double moneyOnDeath = 2;
+    private double moneyOnKill;
+    private double moneyOnDeath;
+    Setup s = new Setup();
 
     @Override
     public void onLoad() {
@@ -36,13 +30,13 @@ public class Eco extends JavaPlugin implements Listener{
 
     @Override
     public void onEnable(){
-        if (!setupEconomy()) {
+        if (!s.setupEconomy()) {
             logger.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
-        setupPermissions();
-        setupChat();
+        s.setupPermissions();
+        s.setupChat();
         logger.info(String.format("[%s] - Plugin enabled. Vault dependency found.", getDescription().getName()));
         getServer().getPluginManager().registerEvents(this, this);
     }
@@ -53,36 +47,11 @@ public class Eco extends JavaPlugin implements Listener{
         getLogger().info(String.format("[%s] - Plugin disabled.", getDescription().getName()));
     }
 
-
     public void reloadPlugin(){
         this.reloadConfig();
         logger.info(String.format("[%s] - Configuration reloaded.", getDescription().getName()));
         moneyOnKill = getConfig().getDouble("Money.OnKill");
         moneyOnDeath = getConfig().getDouble("Money.OnDeath");
-    }
-
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
-    }
-
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = rsp.getProvider();
-        return perms != null;
     }
 
     @EventHandler
@@ -92,21 +61,24 @@ public class Eco extends JavaPlugin implements Listener{
 
         if (killer == null){
             player.sendMessage(ChatColor.DARK_AQUA + "You were killed by the environment and lost " + ChatColor.GOLD + "$" + moneyOnDeath);
-            econ.withdrawPlayer(player, moneyOnDeath);
+            s.econ.withdrawPlayer(player, moneyOnDeath);
         }else{
             killer.sendMessage(ChatColor.DARK_AQUA + "You killed " + player.getDisplayName() + " and earned " + ChatColor.GOLD + "$" + moneyOnKill);
-            econ.depositPlayer(killer, moneyOnKill);
+            s.econ.depositPlayer(killer, moneyOnKill);
         }
     }
 
-    public void onCommand(CommandSender sender, Command cmd, String[]args){
+    public boolean onCommand(CommandSender sender, Command cmd, String[]args){
         if (cmd.getName().equalsIgnoreCase("ecoreload")){
             if (sender.hasPermission("ecokill.reload")){
                 sender.sendMessage(ChatColor.DARK_AQUA + "Configuration reloaded.");
                 reloadPlugin();
+                return true;
             }else{
                 sender.sendMessage(ChatColor.RED + "Insufficient permissions to execute command.");
+                return false;
             }
         }
+        return false;
     }
 }
---
+++ src/main/java/io/github/rsmake/EcoKill/Setup.java
@@ -0,0 +1,38 @@
+package io.github.rsmake.EcoKill;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Setup extends JavaPlugin{
+
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
+
+    public boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    public boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+}
